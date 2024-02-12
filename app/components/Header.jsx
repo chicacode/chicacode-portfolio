8a346a4f5cb043dabca9bf3465a0c194dfae5f95
chicacode <gerahernan.al@gmail.com>
2024-02-11 19:45:09 -0800

    first commit

---
+++ app/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hello, I&apos;m Geri
+          </h1>
+          <p className="text-[#ADB7EB] text-lg mb-6 lg:text-xl">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry&apos;s standard dummy
+            text ever since the 1500s,
+          </p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+                Work With Me
+            </button>
+            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+                Download Resume
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/mask-image.png"
+              width={300}
+              height={300}
+              alt="Picture of the author"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
